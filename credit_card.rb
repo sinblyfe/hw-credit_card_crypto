48060933e0261ab3908d70868631f73a55d4098b
sinblyfe <sinblyfe@gmail.com>
2026-03-12 20:45:55 +0800

    Final code

--- credit_card.rb
+++ credit_card.rb
@@ -5,18 +5,30 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
+
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+    
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
+      
     }.to_json
   end
 
@@ -28,6 +40,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    ruby_hash = JSON.parse(card_s)
+    CreditCard.new(ruby_hash['number'], ruby_hash['expiration_date'], ruby_hash['owner'], ruby_hash['credit_network'])
   end
 
   # return a cryptographically secure hash
@@ -35,5 +49,8 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    require 'digest'
+    Digest::SHA256.hexdigest(to_json)
+
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,32 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    # reverse nums_a 
+    nums_rev = nums_a.reverse
+
+    # iterate through the reversed array, n double every second digit, from idx 0
+    nums_rev.each_with_index do |num, idx|
+      if idx.odd?
+        nums_rev[idx] = num * 2
+        # if doubling result > 9, subtract by 9
+        if nums_rev[idx] > 9
+          nums_rev[idx] -= 9
+        end
+      end
+    end
+
+    # sum all the digits
+      total_num = nums_rev.sum
+
+      # if sum % 10 == 0, return true, else false
+      rem = total_num % 10
+
+      if rem == 0 
+        return true
+      else 
+        return false
+      end
+
+
   end
 end
